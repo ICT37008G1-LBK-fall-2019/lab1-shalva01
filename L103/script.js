9051c9bd1b39b34bf8604dd9b89c2f0e9ba245db
nika gabodze <gabodzenika@gmail.com>
2019-10-02 16:05:48 +0400

    lab 1

---
+++ L103/script.js
@@ -0,0 +1,8 @@
+var besika =prompt("what is your name");
+var givia ="baaro brat";
+if(besika=="jemala"){
+    document.write ("mevar besika zdarovabrat")
+}
+else {
+    document.write ("go away 3:)")
+}
